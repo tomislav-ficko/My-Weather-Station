9ec82f77a59d231aa3bbdbd782e5aec815105736
tomislav-ficko <ficko.tomislav@gmail.com>
2022-04-13 14:35:50 +0200

    Add application class

---
+++ app/src/main/java/com/ingemark/ficko/myweatherstation/MyWeatherStationApplication.kt
@@ -0,0 +1,7 @@
+package com.ingemark.ficko.myweatherstation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyWeatherStationApplication : Application()
