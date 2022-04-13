ae0afa011a5cc618988f67f765251d98ae0f7c47
tomislav-ficko <ficko.tomislav@gmail.com>
2022-04-13 14:46:17 +0200

    Add domain model

---
+++ app/src/main/java/com/ingemark/ficko/myweatherstation/model/WeatherInfo.kt
@@ -0,0 +1,9 @@
+package com.ingemark.ficko.myweatherstation.model
+
+class WeatherInfo {
+    var locationName: String? = null
+    var temperature: Float? = null
+    val windSpeed: Float? = null
+    val humidity: Int? = null
+    val pressure: Float? = null
+}
